d9d3de4534129fd5ec67060e9f531b0b8362f1a6
avash04 <avashn04@gmail.com>
2025-09-22 16:35:14 +1000

    working code added

---
+++ code.js
@@ -0,0 +1,16 @@
+function diceRoll(){
+const imgDisplay=document.getElementById("imgDisplay");
+const resultDisplay=document.getElementById("resultDisplay");
+const numSelector=document.getElementById("numSelector").value;
+
+const img=[];
+const result=[];
+
+for(let i=0;i<numSelector;i++){
+const values= Math.floor((Math.random()*6)+1);
+result.push(values);
+img.push(`<img src="img/${values}.png" alt="dice">`);
+}
+resultDisplay.textContent=`dice:${result.join(",")}`;
+imgDisplay.innerHTML = img.join(" ");
+}
